6266b5cb2e258f9fdb0da1d71aadc0deb207bcf5
Getoari <getoari_g@hotmail.com>
2026-07-23 14:28:09 +0200

    added product and order model and database schema

---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'total',
+    ];
+
+    protected $casts = [
+        'total' => 'decimal:2',
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'sku',
+        'price',
+        'stock_quantity',
+        'category',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'stock_quantity' => 'integer',
+    ];
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
